4b74504c09bfa06abf9604864b9ba9adedc62796
mykkarp <mykkarp@gmail.com>
2022-01-05 12:50:42 +0300

    init: initialize project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef } from 'react';
+import styles from './App.module.css';
 
 function App() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const canvasContainerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current as HTMLCanvasElement;
+    const canvasContainer = canvasContainerRef.current as HTMLDivElement;
+    canvas.width = canvasContainer.clientWidth;
+    canvas.height = canvasContainer.clientHeight;
+    canvas.style.width = canvasContainer.clientWidth + 'px';
+    canvas.style.height = canvasContainer.clientHeight + 'px';
+
+    const context = canvas.getContext('2d') as CanvasRenderingContext2D;
+    context.fillRect(200, 50, 200, 50);
+
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className={styles.wrapper}>
+      <header>header</header>
+      <main>
+        <div
+          ref={canvasContainerRef}
+          className={styles.canvasContainer}
         >
-          Learn React
-        </a>
-      </header>
+          <canvas
+            ref={canvasRef}
+          />
+        </div>
+      </main>
+      <footer>footer</footer>
     </div>
   );
 }
---
+++ styles.d.ts
@@ -0,0 +1,4 @@
+declare module "*.module.css" {
+  const asses: { [key: string]: string };
+  export default asses;
+}
